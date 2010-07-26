794b806e90fd068616350644a07443e182b55553
artes.s <artes.s@gmail.com>
2010-07-26 15:27:24 +0000

    Added DriveToolBar

--- TestForm/Form1.Designer.cs
+++ TestForm/Form1.Designer.cs
@@ -28,21 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.fileViewPanel1 = new Commander.FileViewPanel();
+            this.driveToolBar1 = new TestForm.DriveToolBar();
             this.SuspendLayout();
             // 
             // fileViewPanel1
             // 
-            this.fileViewPanel1.Location = new System.Drawing.Point(39, 26);
+            this.fileViewPanel1.Location = new System.Drawing.Point(423, 38);
             this.fileViewPanel1.Name = "fileViewPanel1";
             this.fileViewPanel1.Size = new System.Drawing.Size(343, 240);
             this.fileViewPanel1.TabIndex = 0;
+            // 
+            // driveToolBar1
+            // 
+            this.driveToolBar1.Appearance = System.Windows.Forms.ToolBarAppearance.Flat;
+            this.driveToolBar1.AutoSize = false;
+            this.driveToolBar1.ButtonSize = new System.Drawing.Size(36, 21);
+            this.driveToolBar1.DropDownArrows = true;
+            this.driveToolBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.driveToolBar1.Location = new System.Drawing.Point(0, 0);
+            this.driveToolBar1.Name = "driveToolBar1";
+            this.driveToolBar1.ShowToolTips = true;
+            this.driveToolBar1.Size = new System.Drawing.Size(996, 32);
+            this.driveToolBar1.TabIndex = 1;
+            this.driveToolBar1.TextAlign = System.Windows.Forms.ToolBarTextAlign.Right;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 290);
+            this.ClientSize = new System.Drawing.Size(996, 290);
+            this.Controls.Add(this.driveToolBar1);
             this.Controls.Add(this.fileViewPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -53,6 +70,7 @@
         #endregion
 
         private Commander.FileViewPanel fileViewPanel1;
+        private DriveToolBar driveToolBar1;
 
 
 
